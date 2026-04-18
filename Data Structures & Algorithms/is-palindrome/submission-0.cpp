42b7a395185a10fca0cff7f967e0da92d0d5b5a8
Mike Auslander <michael.auslander+github@gmail.com>
2026-04-18 19:53:20 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        auto front = s.begin();
+        auto back = s.end() - 1;
+
+
+
+
+
+        while (front < back) {
+
+            while (!isalnum(*front) && front != back) {
+                front++;
+            }
+            while (!isalnum(*back) && front != back) {
+                back--;
+            }
+
+            if (tolower(*front) != tolower(*back)) return false;
+            front++;
+            back--;
+        }
+        return true;
+    }
+};
